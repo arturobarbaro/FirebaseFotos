ff86de0703c5855977dc11790f0bca1906f5945f
Arturo Barba <arturo.barba@iesdonana.org>
2019-03-19 12:20:51 +0100

    Carga de imagenes a Firebase

--- src/app/providers/carga-imagenes.service.ts
+++ src/app/providers/carga-imagenes.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { FileItem } from '../models/file-item';
+import * as firebase from 'firebase';
 
 
 @Injectable({
@@ -13,7 +14,31 @@ export class CargaImagenesService {
   constructor(private db:AngularFirestore) { }
 
   cargarImagenesFirebase(imagenes: FileItem[]){
-      console.log(imagenes)
+      const storageRef=firebase.storage().ref();
+      for (let item of imagenes) {
+        item.estaSubiendo=true;
+        if(item.progreso >= 100){
+            continue;
+        }
+        const uploadTask: firebase.storage.UploadTask =
+        storageRef.child(`${this.CARPETA_IMAGENES}/${item.nombreArchivo}`)
+                .put(item.archivo);
+
+        uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED,
+            (snapshot: firebase.storage.UploadTaskSnapshot) =>item.progreso = (snapshot.bytesTransferred/snapshot.totalBytes)*100,
+            (error)=>console.error('Error al subir...'),
+            ()=>{
+                console.log('Imagen cargada')
+                item.url=uploadTask.snapshot.downloadURL;
+                item.estaSubiendo=false;
+                this.guardarImagen({
+                    nombre: item.nombreArchivo,
+                    url: item.url
+                })
+            }
+
+        )
+      }
   }
 
   private guardarImagen(imagen: {nombre:string, url: string}){
