c68b14403c926a88c9156ef6e4db48db4f8b847b
Arturo Barba <arturo.barba@iesdonana.org>
2019-03-19 11:42:59 +0100

    Validaciones

--- src/app/directives/ng-drop-files.directive.ts
+++ src/app/directives/ng-drop-files.directive.ts
@@ -1,10 +1,12 @@
 import { Directive, EventEmitter, ElementRef, HostListener, Input, Output } from '@angular/core';
+import { FileItem } from '../models/file-item';
 
 @Directive({
   selector: '[appNgDropFiles]'
 })
 export class NgDropFilesDirective {
 
+  @Input() archivos:FileItem[] = [];
   @Output() mouseSobre: EventEmitter<boolean> = new EventEmitter();
 
 
@@ -18,7 +20,31 @@ export class NgDropFilesDirective {
 
   @HostListener('gragleave', ['$evenet'])
   public ondragLeave(event: any){
-      this.mouseSobre.emit(false);  
+      this.mouseSobre.emit(false);
+  }
+
+  //Validaciones
+  private _archivoPuedeSerCargado(archivo:File):boolean{
+      (!this._archivoYaFueDrp(archivo.name)&&this._esImagen(archivo.type))?true:false;
+  }
+
+  private _prevenirDetener(e){
+      e.preventDefault();
+      e.stopPropagation();
+  }
+
+  private _archivoYaFueDrp(nombreArchivo: string):boolean{
+      for (let archivo of this.archivos) {
+          if(archivo.nombreArchivo==nombreArchivo){
+              console.log(`El archivo ${nombreArchivo} ya fué añadido`);
+              return true;
+          }
+      }
+      return false;
+  }
+
+  private _esImagen(tipoArchivo: string):boolean{
+      (tipoArchivo==''||tipoArchivo==undefined)?false:tipoArchivo.startsWith('image');
   }
 
 }
